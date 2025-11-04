1a2efc5d6247dc52325787f473c483619ebfe0cd
salma akg <salmaakagou1@gmail.com>
2025-11-04 12:42:20 +0100

    Ajout des factories et seeders pour articles, tags et utilisateurs

--- prototype/database/seeders/DatabaseSeeder.php
+++ prototype/database/seeders/DatabaseSeeder.php
@@ -21,5 +21,11 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        $this->call([
+            UserSeeder::class,
+            TagSeeder::class,
+            ArticleSeeder::class,
+            pivoteArticleTagSeeder::class,
+        ]);
     }
 }
