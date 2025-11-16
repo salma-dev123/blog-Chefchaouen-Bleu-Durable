9a5d639fef3255b84867ef4709eda06f7f4b7d08
salma akg <salmaakagou1@gmail.com>
2025-11-16 19:48:02 +0100

    ajout prototype

---
+++ prototype/database/seeders/PivoteArticleCategorySeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Article;
+use App\Models\Category;
+
+class PivoteArticleCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categoriesId = Category::pluck('id');
+        
+        Article::all()->each(function($article) use ($categoriesId) {
+            $article->categories()->sync(
+                $categoriesId->random(rand(1, 4))->all()
+            );
+        });
+    }
+}
