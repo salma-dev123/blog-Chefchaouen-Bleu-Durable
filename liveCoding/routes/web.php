5644686147a2e270125bc9e22c1a98d5d178cbaf
salma akg <salmaakagou1@gmail.com>
2025-11-17 13:25:09 +0100

    ajout de live Coding de prototype

---
+++ liveCoding/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+
+
+Route::get('/articles', [ArticleController::class, 'index'])->name('articles.index');
+Route::delete('/articles/{article}', [ArticleController::class, 'destroy'])->name('articles.destroy');
+
+
+
